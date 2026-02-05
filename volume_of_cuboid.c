4b680fcc3e1a18b759d99bae8be0295542551103
Manuratnam <manuratnammj@gmail.com>
2026-02-05 21:52:48 +0530

    Add volume_of_cuboid.c to calculate the volume of a cuboid

---
+++ volume_of_cuboid.c
@@ -0,0 +1,12 @@
+//volume of quboid
+/*Manuratnam MJ
+ reg.no:25101073*/
+#include<stdio.h>
+void main()
+{
+    int l,b,h,vol;
+    printf("enter length,breath,height");
+    scanf("%d %d %d",&l, &b, &h);
+    vol=l*b*h;
+    printf("volume of quboid is:%d",vol);
+}
