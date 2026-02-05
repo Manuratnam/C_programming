56888cbf367302ab955856613ceb824ce3626edf
Manuratnam <manuratnammj@gmail.com>
2026-02-05 21:45:37 +0530

    Add Area_of_rectangle.c to calculate the area of a rectangle

---
+++ Area_of_rectangle.c
@@ -0,0 +1,13 @@
+//Area of a rectangle
+/*Manuratnam MJ
+ reg.no:25101073*/
+#include <stdio.h>
+
+void main()
+{
+    int l,b,area;
+    printf("enter length, breadth");
+    scanf("%d %d",&l,&b);
+    area=l*b;
+    printf("the area of rectangle is:%d",area);
+}
