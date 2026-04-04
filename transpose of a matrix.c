89a5fefa768bc6fe15c9ed045556061e74f2f042
Manuratnam <manuratnammj@gmail.com>
2026-04-04 16:46:06 +0530

    added prgrm to rewad and print transpose of a matrix

---
+++ transpose of a matrix.c
@@ -0,0 +1,33 @@
+//transpose of a matrix
+#include<stdio.h>
+int main()
+{
+    int arr[3][3],i,j;
+    printf("enter num:");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("original matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Transpose of the matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",arr[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
